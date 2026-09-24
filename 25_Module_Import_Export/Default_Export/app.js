e12e01a7eef3114ebf5f59479dee2b9c6cfd1b11
Ronald Castromero <callme14noy@gmail.com>
2026-09-24 22:23:53 +0800

    added item

---
+++ 25_Module_Import_Export/Default_Export/app.js
@@ -0,0 +1,6 @@
+// --- file: app.js ---
+// Import the default function
+import greet from "./greet.js";
+
+// Use the imported function
+console.log(greet("Sara"));
---
+++ 25_Module_Import_Export/Default_Export/greet.js
@@ -0,0 +1,5 @@
+// --- file: greet.js ---
+// Export a default function
+export default function greet(name) {
+  return 'Hello, ${name}!';
+}
